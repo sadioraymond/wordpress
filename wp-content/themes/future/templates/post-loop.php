ee3de93dd0975905aa27fbfe01b0c444875b9f87
sadioraymond <sadiowalyraymond@gmail.com>
2017-01-04 13:45:16 +0000

    Modification de post-loop

--- wp-content/themes/future/templates/post-loop.php
+++ wp-content/themes/future/templates/post-loop.php
@@ -1,17 +1,20 @@
 						<!-- Post -->
+							<?php if (have_posts() ) :
+								while (have_posts()) :the_post();
+							?>
 							<article class="post">
 								<header>
 									<div class="title">
-										<h2><a href="#">Article 1</a></h2>
+										<h2><a href="<?php the_permalink(); ?>"><?php the_title();?> 1</a></h2>
 										<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
 									</div>
 									<div class="meta">
-										<time class="published" datetime="2015-11-01">November 1, 2015</time>
-										<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
+										<time ><?php the_time('d-m-y'); ?></time>
+										<a href="#" class="author"><span class="name"><?php the_author(); ?></span><img src="images/avatar.jpg" alt="" /></a>
 									</div>
 								</header>
 								<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+								<p><?php the_excerpt(); ?></p>
 								<footer>
 									<ul class="actions">
 										<li><a href="#" class="button big">Continue Reading</a></li>
@@ -23,6 +26,6 @@
 									</ul>
 								</footer>
 							</article>
-
+	<?php endwhile; endif ?>
 						<!-- Post -->
 				
