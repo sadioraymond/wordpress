551bf14db91fe327715b2559bbdd298f2ff957c1
sadioraymond <sadiowalyraymond@gmail.com>
2017-01-04 10:46:55 +0000

    Ajout des fonctiod concernant internet explorer dans la fonction future_scripts de functions.php

--- wp-content/themes/future/functions.php
+++ wp-content/themes/future/functions.php
@@ -35,7 +35,17 @@ add_action( 'after_setup_theme', 'future_setup' );
  */
 
 function future_scripts() {
-wp_enqueue_style( 'future-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'future-style', get_stylesheet_uri() );
+    // Load the Internet Explorer 8 specific stylesheet
+	wp_enqueue_style( 'future-ie8', get_theme_file_uri('assets/css/ie8.css'), array( 'future-style' ), '1.0' );
+	wp_style_add_data( 'future-ie8', 'conditional', 'lte IE 8' );
+    //Html5shiv
+    wp_enqueue_script( 'future-ie8-shiv', get_theme_file_uri('assets/js/ie/html5shiv.js'), array(), '3.7.3' );
+	wp_script_add_data( 'future-ie8-shiv', 'conditional', 'lte IE 9' );
+    // Load the Internet Explorer 9 specific stylesheet, to fix display issues in the Customizer.
+    wp_enqueue_style( 'future-ie9', get_theme_file_uri('assets/css/ie9.css'), array( 'future-style' ), '1.0' );
+	wp_style_add_data( 'future-ie9', 'conditional', 'IE 9' );
+
 }
 add_action( 'wp_enqueue_scripts', 'future_scripts' );
  ?>
